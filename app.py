8b533d31e51d69f19e245fd688b24094f8d74bb7
ALynch17 <123436712+ALynch17@users.noreply.github.com>
2023-02-03 15:03:15 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
-
 app = FastAPI()
 
-origins=[
+origins = [
     "https://ecse-week3-demo.netlify.app"
 ]
 
-fake_database=[]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-
+fake_database = []
 
 @app.get("/todos")
 async def get_all_todos():
-    return fake_database
+  return fake_database
 
 @app.post("/todos")
 async def create_todo(request: Request):
-    todo = await request.json()
-    fake_database.append(todo)
-    return todo
-
+  todo = await request.json()
+  fake_database.append(todo)
+  return todo
